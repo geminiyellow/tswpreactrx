b747d5ca1b13c3d092db5138bf715d0fe2a0423a
olegvm <megabooper@hotmail.com>
2016-03-08 21:39:24 +0300

    Tons of linting fixed

--- App/App1.tsx
+++ App/App1.tsx
@@ -4,29 +4,38 @@ import * as Rx from "rx";
 import * as React from "react";
 import {createRxComponent, funcSubject} from "react-rx-component";
 
-const { CounterContainer } = createRxComponent<number>((props$: Rx.Observable<{}>) => {
-  const increment$ = funcSubject<number>(); // handleIncrement
-  const count$ = increment$
-    .startWith(0) // state = { count: 0 }
-    .scan<number>((count: number) => count + 1); // this.setState((state) => ({ count: count + 1 }))
+interface IProps {
+    count:  number;
+    increment: () => void;
+}
 
-  return Rx.Observable.combineLatest<any, number, any>(props$, count$, (props: {}, count: number) => ({ props, increment: increment$, count }));
-}, Counter);
-
-function Counter(props) {
-  const { count, increment } = props;
+let counterView: (props: IProps) => JSX.Element = (props: IProps) => {
   return (
     <div>
-      {count}
-      <button onClick={increment}>Increment</button>
+      <span>{props.count}</span>
+      <button onClick={props.increment}>Click me</button>
     </div>
   );
-}
+};
+
+const { CC }: JSX.ElementClass = createRxComponent<number>(
+    (props$: Rx.Observable<{}>) => {
+        const increment$: Rx.Observable<number> = funcSubject<number>();
+        const count$: Rx.Observable<number> = increment$.startWith(0).scan<number>((count: number) => count + 1);
+
+        return Rx.Observable.combineLatest<{}, number, {}>(
+            props$,
+            count$,
+            (props: {}, count: number) => (
+                { props, increment: increment$, count }
+            ));
+    },
+    counterView);
 
 export default class App extends React.Component<{}, {}> {
     render(): React.ReactElement<{}> {
         return  <div>
-                    <CounterContainer />
+                    <CC />
                 </div>;
     }
 }
